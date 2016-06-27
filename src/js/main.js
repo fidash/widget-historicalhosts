a4933627209f3cb3088c56a08b47a4fe171fc081
mgarcia <miguelglafuente@gmail.com>
2016-06-27 10:37:16 +0200

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+/*
+ * widget-historicalhosts
+ * https://github.com/fidash/widget-historicalhosts
+ *
+ * Copyright (c) 2016 CoNWeT
+ * Licensed under the Apache-2.0 license.
+ */
+
+/* globals WidgetHistoricalHosts */
+
+window.onload = function () {
+    "use strict";
+    new WidgetHistoricalHosts();
+};
